7e9b1fa205b43c69fe85746e9d51e3dc0d033f6b
ghightower80 <ghightower80@yahoo.com>
2024-02-15 22:19:25 -0500

    first

---
+++ db.js
@@ -0,0 +1,11 @@
+//db setup//
+const { Client } = require("pg");
+
+const client = new Client({
+    connectionString: "postgresql:///biztime"
+});
+
+client.connect();
+
+
+module.exports = client;
---
+++ server.js
@@ -0,0 +1,8 @@
+//server startup//
+
+const app = require("./app");
+
+
+app.listen(3000, function () {
+    console.log("Listening on 3000");
+});
