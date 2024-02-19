221c3833ec2674fcbff507621b34ee49f43d48ec
ghightower80 <ghightower80@yahoo.com>
2024-02-19 12:30:02 -0500

    updated

--- routes/companies.js
+++ routes/companies.js
@@ -1,6 +1,5 @@
 // Routes for companies//
 
-
 const express = require("express");
 const slugify = require("slugify");
 const ExpressError = require("../expressError")
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,6 +1,5 @@
 // Routes for invoices//
 
-
 const express = require("express");
 const ExpressError = require("../expressError")
 const db = require("../db");
